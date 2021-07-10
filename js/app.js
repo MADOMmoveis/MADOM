a85bae12e04bd71a8317b4d4f5905b0585fa8daa
a7madyousef <dawabsheh@engineer.com>
2021-07-10 05:23:19 +0300

    redesigned homepage added sliders and navbar

---
+++ js/app.js
@@ -0,0 +1,34 @@
+/* swiper start */
+var swiper = new Swiper( '.swiper-container.two', {
+    autoplay: 2500,
+    effect: 'coverflow',
+    loop: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflow: {
+        rotate: 0,
+        stretch: 100,
+        depth: 150,
+        modifier: 1.5,
+        slideShadows : false,
+
+    }
+} );
+
+var swiper2 = new Swiper( '.ratedslider', {
+    loop: true,
+    centeredSlides: true,
+    slidesPerView: '7',
+    effect: 'coverflow',
+    coverflow: {
+        rotate: 0,
+        stretch: 0,
+        depth: 0,
+        modifier: 1.5,
+        slideShadows : false,
+    }
+    
+} );
+
+
+/* swiper end */
